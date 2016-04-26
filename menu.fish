9844a0dd0405ddbaeea6a12c6d2f5c9257a18cf1
Jorge Bucaran <j@bucaran.me>
2016-04-27 07:28:43 +0900

    Return each selected index in multiple choice mode, fix weird extra padding and change menu_focused_ to menu_hover_

--- menu.fish
+++ menu.fish
@@ -1,4 +1,8 @@
 function -S menu
+    if test -z "$argv"
+        return 1
+    end
+
     function __menu_move_print -a row col
         set -e argv[1..2]
         printf "\x1b[$row;$col""f$argv"
@@ -18,42 +22,51 @@ function -S menu
 
     function -S __menu_draw_items -a row_index
         set -e argv[1]
+        set -l before ""
 
         for i in (seq $item_count)
-            if test ! -z "$multiple"
-              if contains $i $menu_selected_index
-                set before "$checked_glyph"
-              else
-                set before "$unchecked_glyph"
-              end
+            if test "$multiple" = true
+                if contains -- "$i" $menu_selected_index
+                    set before "$checked_glyph "
+                else
+                    set before "$unchecked_glyph "
+                end
             end
+
             if test "$i" = "$row_index"
                 set -l glyph "$cursor_glyph_style$cursor_glyph$normal"
-                set -l item "$focused_item_style$argv[$i]$normal"
+                set -l item "$hover_item_style$argv[$i]$normal"
 
-                # if it is selected display checkbox
-                __menu_move_print $i 1 " $glyph $before $item"
+                __menu_move_print $i 1 "$glyph $before$item"
             else
                 set -l item "$argv[$i]"
-                if contains $i $menu_selected_index
-                  set item "$selected_item_style$argv[$i]$normal"
+
+                if contains -- "$i" $menu_selected_index
+                    set item "$selected_item_style$argv[$i]$normal"
                 end
-                __menu_move_print $i 1 "   $before $item"
+
+                __menu_move_print $i 1 "  $before$item"
             end
         end
     end
 
     set menu_selected_index
-    set menu_focused_item
+
+    set -l cursor_glyph ">"
+    set -l cursor_glyph_style
 
     set -l checked_glyph "[x]"
     set -l unchecked_glyph "[ ]"
-    set -l cursor_glyph ">"
-    set -l cursor_glyph_style
-    set -l focused_item_style
+
+    set -l hover_item_style
     set -l selected_item_style
+
+    set -l multiple "$menu_multiple_choice"
     set -l normal
-    set -l multiple
+
+    if test ! -z "$multiple" -a "$multiple" != false
+        set multiple true
+    end
 
     if test ! -z "$menu_cursor_glyph"
         set cursor_glyph "$menu_cursor_glyph"
@@ -64,18 +77,14 @@ function -S menu
         set normal (set_color normal)
     end
 
-    if test ! -z "$menu_focused_item_style"
-        set focused_item_style (set_color $menu_focused_item_style)
+    if test ! -z "$menu_hover_item_style"
+        set hover_item_style (set_color $menu_hover_item_style)
         set normal (set_color normal)
     end
 
     if test ! -z "$menu_selected_item_style"
         set selected_item_style (set_color $menu_selected_item_style)
         set normal (set_color normal)
-    end
-
-    if test $menu_multiple_choices -eq 1
-        set multiple 1
     end
 
     if test ! -z "$menu_checked_glyph"
@@ -101,19 +110,21 @@ function -S menu
 
         switch "$c"
             case " "
-                if test ! -z "$multiple"
-                  if contains $row_index $menu_selected_index
-                    set -e menu_selected_index[(contains --index $row_index $menu_selected_index)]
-                  else
-                    set menu_selected_index $menu_selected_index "$row_index"
-                  end
+                if test "$multiple" = true
+                    if contains $row_index $menu_selected_index
+                        set -e menu_selected_index[(contains --index "$row_index" $menu_selected_index)]
+                    else
+                        set menu_selected_index $menu_selected_index "$row_index"
+                    end
                 end
 
             case ""
-                set menu_selected_index "$row_index"
-                set menu_focused_item "$argv[$row_index]"
+                if test -z "$menu_selected_index"
+                    set menu_selected_index "$row_index"
+                end
 
                 __menu_fullscreen --leave
+
                 break
 
             case "["
@@ -122,19 +133,21 @@ function -S menu
                 switch "$c"
                     case A
                         set row_index (echo $row_index - 1 | bc)
+
                         if test "$row_index" -le 0
                             set row_index 1
                         end
 
                     case B
                         set row_index (echo $row_index + 1 | bc)
-                        if test "$row_index" -ge $item_count
+
+                        if test "$row_index" -ge "$item_count"
                             set row_index $item_count
                         end
                 end
         end
 
-        clear
+        command clear
 
         __menu_draw_items $row_index $argv
     end
